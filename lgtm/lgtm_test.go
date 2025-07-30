ba44537a16cbf4d6a1438d68cc75433fcb144aa3
Kristopher Lam <root@sandb0x.io>
2025-07-30 15:33:49 +0800

    added tests for misc modules

--- lgtm/lgtm_test.go
+++ lgtm/lgtm_test.go
@@ -7,6 +7,11 @@ import (
 
 	container "github.com/narwhl/mockestra/lgtm"
 	"github.com/testcontainers/testcontainers-go"
+	"go.opentelemetry.io/contrib/bridges/otelslog"
+	"go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploghttp"
+	"go.opentelemetry.io/otel/sdk/log"
+	"go.opentelemetry.io/otel/sdk/resource"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
 	"go.uber.org/fx"
 	"go.uber.org/fx/fxtest"
 )
@@ -31,7 +36,32 @@ func TestLGTMModule(t *testing.T) {
 			fx.In
 			Container testcontainers.Container `name:"lgtm"`
 		}) {
-			t.Log("To be implemented: LGTM module test")
+			endpoint, err := params.Container.PortEndpoint(t.Context(), container.OtlpHttpPort, "")
+			if err != nil {
+				t.Fatalf("Failed to get LGTM container endpoint: %v", err)
+			}
+			logExporter, err := otlploghttp.New(
+				t.Context(),
+				otlploghttp.WithInsecure(),
+				otlploghttp.WithEndpoint(endpoint),
+			)
+			if err != nil {
+				t.Fatalf("Failed to create OTLP log exporter: %v", err)
+			}
+			r := resource.NewWithAttributes(
+				semconv.SchemaURL,
+				semconv.ServiceNameKey.String("lgtm-test"),
+			)
+			processor := log.NewBatchProcessor(logExporter)
+			provider := log.NewLoggerProvider(
+				log.WithProcessor(processor),
+				log.WithResource(r),
+			)
+			logger := otelslog.NewLogger(
+				"lgtm-test",
+				otelslog.WithLoggerProvider(provider),
+			)
+			logger.Info("LGTM container is ready")
 		}),
 	)
 	app.RequireStart()
