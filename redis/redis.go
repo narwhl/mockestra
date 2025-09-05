e958c2e2877748ea8cdd0aa43685a889576775ee
Kristopher Lam <root@sandb0x.io>
2025-09-05 16:48:15 +0800

    feat(redis): add Redis container module and test

---
+++ redis/redis.go
@@ -0,0 +1,105 @@
+package redis
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"time"
+
+	"github.com/narwhl/mockestra"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
+	"go.uber.org/fx"
+)
+
+const (
+	Tag   = "redis"
+	Image = "redis"
+	Port  = "6379/tcp"
+
+	ContainerPrettyName = "Redis"
+)
+
+type RequestParams struct {
+	fx.In
+	Prefix  string                               `name:"prefix"`
+	Version string                               `name:"redis_version"`
+	Opts    []testcontainers.ContainerCustomizer `group:"redis"`
+}
+
+func New(p RequestParams) (*testcontainers.GenericContainerRequest, error) {
+	r := testcontainers.GenericContainerRequest{
+		ContainerRequest: testcontainers.ContainerRequest{
+			Name:         fmt.Sprintf("mock-%s-%s", p.Prefix, Tag),
+			Image:        fmt.Sprintf("%s:%s", Image, p.Version),
+			ExposedPorts: []string{Port},
+			Env:          make(map[string]string),
+			WaitingFor:   wait.ForListeningPort(Port).WithStartupTimeout(time.Second * 10),
+		},
+		Started: true,
+	}
+
+	for _, opt := range p.Opts {
+		if err := opt.Customize(&r); err != nil {
+			return nil, err
+		}
+	}
+
+	return &r, nil
+}
+
+type ContainerParams struct {
+	fx.In
+	Lifecycle fx.Lifecycle
+	Request   *testcontainers.GenericContainerRequest `name:"redis"`
+}
+
+type Result struct {
+	fx.Out
+	Container      testcontainers.Container `name:"redis"`
+	ContainerGroup testcontainers.Container `group:"containers"`
+}
+
+func Actualize(p ContainerParams) (Result, error) {
+	c, err := testcontainers.GenericContainer(context.Background(), *p.Request)
+	if err != nil {
+		return Result{}, fmt.Errorf("an error occurred while instantiating %s container: %w", ContainerPrettyName, err)
+	}
+
+	p.Lifecycle.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			redisEndpoint, err := c.Endpoint(ctx, "")
+			if err != nil {
+				return fmt.Errorf("an error occurred while querying %s endpoint: %w", ContainerPrettyName, err)
+			}
+			slog.Info(fmt.Sprintf("%s container is running at", ContainerPrettyName), "addr", redisEndpoint)
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			err := c.Terminate(ctx)
+			if err != nil {
+				slog.Warn(fmt.Sprintf("an error occurred while terminating %s container", ContainerPrettyName), "error", err)
+			} else {
+				slog.Info(fmt.Sprintf("%s container is terminated", ContainerPrettyName))
+			}
+			return err
+		},
+	})
+	return Result{
+		Container:      c,
+		ContainerGroup: c,
+	}, nil
+}
+
+var WithPostReadyHook = mockestra.WithPostReadyHook
+
+var Module = mockestra.BuildContainerModule(
+	"redis",
+	fx.Provide(
+		fx.Annotate(
+			New,
+			fx.ResultTags(`name:"redis"`),
+		),
+		Actualize,
+	),
+)
